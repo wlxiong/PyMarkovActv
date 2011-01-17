cca7c79b7a6f39f6d0b3d57501ffabd477595638
Yiliang Xiong <wlxiong@gmail.com>
2011-01-17 16:21:46 +0800

    abandon assign_test.py, the program start from main.py

--- main.py
+++ main.py
@@ -1,45 +1,29 @@
-# stochastic discrete choice model
-## import pycallgraph
-## pycallgraph.start_trace()
-import iofile.output
+# Markov decision process
+from iofile.inputs import load_network, load_activity
+from iofile.outputs import export_data
+from loading.assign import find_fixed_point
+from viewer.plot import draw_zone_population
+from allocating.generators import gen_activity_util, gen_path_set
 
-def sim_rand_assign(N, case):
-    "Generate random utility of activity and assign the passengers to network. "
-    for n in range(N):
-        print '\n*** %d ***' % n
-        init_flow_step(0.0)
-        init_path_flow(10.0)
-        calc_flow_step()
-        update_link_flow(1)
-        # initialize the activities
-    ##     gen_activity_util()
-        gen_rand_activity_util()
-        # find equilibrium flows
-        init_avg_trip()
-        find_fixed_point(num_iter)
-        # export and visualize data
-        export_file_name = case + '-' + str(n)
-        export_data(export_file_name)
-        export_sample_zn_png()
-    
 def main():
-    
-    # load data sets
-    case = '4node'
-    load_network(case)
-    # initialize the transit network
+	# the data set name
+	case_name = '6node'
+	# load activity data
+    load_activity(case_name)
+	gen_activity_util()
+    # load network data
+    load_network(case_name)
     gen_path_set()
-    gen_flow_set()
-    sim_rand_assign(num_sample, case)
-#    draw_zone_passenger(10)
-#    draw_zone_passenger(5)
-#    draw_zone_passenger(2)
-#    plt.show()
-    
-    
+
+	# run the iterative procedure 
+    find_fixed_point(4)
+
+    # output the raw results
+    export_data(case_name)
+	# generate visual results
+#    draw_zone_population(4)
+
 if __name__ == '__main__':
-##     prof = hotshot.Profile("hotshot_stats")
-##     prof.runcall(main)
-##     prof.close()
-#    main()
-##    pycallgraph.make_dot_graph('SA-main.png')
+    import sys
+    sys.path.append('D:\Workspace\PyMarkovActv\src')
+    main()
