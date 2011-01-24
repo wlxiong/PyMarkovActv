c9c80ffbfd6ba4b14f8c9af78620a3b068b32c4d
Yiliang Xiong <wlxiong@gmail.com>
2011-01-24 22:50:46 +0800

    put classes related to location choices in a separate source file

---
+++ networks/location.py
@@ -0,0 +1,25 @@
+# Classes for ctivity locations
+from networks.basic import Node
+
+
+class Zone(Node):
+    "A zone is a area where people participate various activities. "
+    def __init__(self, name, activity_list):
+        super(Zone, self).__init__(name)
+        self.involved_activities = activity_list
+        for each_actv in self.involved_activities:
+            each_actv.add_location(self)
+        
+
+class Home(Zone):
+    "A home is a zone that people can reside at. "
+    def __init__(self, name, activity_list, capacity, rent):
+        super(Home, self).__init__(name, activity_list)
+        self.capacity, self.rent = capacity, rent
+
+
+class Work(Zone):
+    "A work is a place that people find a job. "
+    def __init__(self, name, activity_list, jobs, salary):
+        super(Work, self).__init__(name, activity_list)
+        self.jobs, self.salary = jobs, salary
