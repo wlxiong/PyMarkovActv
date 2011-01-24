5c58473806b713945bd32de1ee3cffde3b8be117
Yiliang Xiong <wlxiong@gmail.com>
2011-01-24 22:54:55 +0800

    minor modification of Alternative

--- choice/model.py
+++ choice/model.py
@@ -3,12 +3,12 @@ import hashlib
 import math
 
 class Alternative(object):
-    """ A basic  class for choice alternatives. 
+    """ A basic class for choice alternatives. 
     """
-    def __init__(self, name, coeff, utility, parent=None, population=None):
+    def __init__(self, name, coeff, utility, parent, volume=None):
         self.name, self.util, self.coeff = name, utility, coeff
         self.parent, self.children = parent, []
-        self.population = population
+        self.volume = volume
         # add this alternative to the children list of its parent
         if self.parent <> None:
             self.parent.add_child(self)
@@ -28,7 +28,7 @@ class Alternative(object):
     def __repr__(self):
         if self.parent == None:
             return "%s(nil)" % (self.name)
-        return "%s(%s)" % (self.name, self.parent.name)
+        return "%s(%s)" % (self.name, self.parent)
 
     def __hash__(self):
         return int(hashlib.md5(repr(self)).hexdigest(), 16)
@@ -56,7 +56,7 @@ class Alternative(object):
             mid_util = self.calc_mid_child_util()
             exp_sum_util = 0.0
             for each_child in self.children:
-                exp_sum_util += math.exp(self.coeff * (each_child.calc_inclusive_value() - self.mid_child_util))
+                exp_sum_util += math.exp(self.coeff * (each_child.calc_inclusive_value() - mid_util))
             log_sum_util = self.mid_child_util + math.log(exp_sum_util) / self.coeff
             self.inclusive_value = log_sum_util + self.util
         return self.inclusive_value
@@ -73,24 +73,24 @@ class Alternative(object):
             self.choice_prob = math.exp(self.parent.coeff * (self.calc_inclusive_value() - mid_util)) / exp_sum_util
         return self.choice_prob
         
-    def calc_population(self):
-        if self.population <> None:
-            return self.population 
+    def calc_choice_volume(self):
+        if self.volume <> None:
+            return self.volume 
         if self.parent == None:
             raise Exception('No enough data for calculate population! ')
         else:
-            total_population = self.parent.calc_population()
-            self.population = total_population * self.calc_choice_prob()
-        return self.population
+            total_volume = self.parent.calc_choice_volume()
+            self.volume = total_volume * self.calc_choice_prob()
+        return self.volume
         
         
 def main():
-    a = Alternative('a', 0.1, 5, None, 2000)
-    b = Alternative('b', 0.2, 5, a)
-    c = Alternative('c', 0.2, 5, a)
-    d = Alternative('d', 0.2, 5, a)
-    e = Alternative('e', 0.3, 5, c)
-    f = Alternative('f', 0.3, 5, c)
+    a = Alternative('a', 0.005, 0.0, None, 2000)
+    b = Alternative('b', 0.01, 5, a)
+    c = Alternative('c', 0.01, 0, a)
+    d = Alternative('d', 0.01, 5, a)
+    e = Alternative('e', 0.05, 5, c)
+    f = Alternative('f', 0.05, 5, c)
     
     print a.children
     print b.children
@@ -116,12 +116,12 @@ def main():
     print  f.calc_choice_prob()
     
     print "calc_population"
-    print a.calc_population()
-    print b.calc_population()
-    print c.calc_population()
-    print d.calc_population()
-    print e.calc_population()
-    print f.calc_population()
+    print a.calc_choice_volume()
+    print b.calc_choice_volume()
+    print c.calc_choice_volume()
+    print d.calc_choice_volume()
+    print e.calc_choice_volume()
+    print f.calc_choice_volume()
     
 if __name__ == '__main__':
     main()
