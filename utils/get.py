ed67dbf5f4ba2ee22313183f9f466e624227ff97
Yiliang Xiong <wlxiong@gmail.com>
2011-02-12 17:03:31 +0800

    change the initialization of flow variables (using try... except...)

--- utils/get.py
+++ utils/get.py
@@ -3,12 +3,16 @@ from shared.universe import flow
 
 def get_move_flow(move):
     " Return the flow with the given name, creating it if necessary. "
-    if move not in flow.movement_flows:
+    try:
+        return flow.movement_flows[move]
+    except KeyError:
         flow.movement_flows[move] = 0.0
-    return flow.movement_flows[move]
+        return flow.movement_flows[move]
 
 def get_move_step(move):
     " Return the step with the given name, creating it if necessary. "
-    if move not in flow.movement_steps:
+    try:
+        flow.movement_steps[move]
+    except KeyError:
         flow.movement_steps[move] = 0.0
-    return flow.movement_steps[move]
+        return flow.movement_steps[move]
